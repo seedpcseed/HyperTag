bab61439a207fb032ed977ea8f713e5ce3e43c7e
seedpcseed <seedpcseed@gmail.com>
2021-09-26 18:59:30 -0500

    updated hypertag.py

--- hypertag/hypertag.py
+++ hypertag/hypertag.py
@@ -2,7 +2,7 @@ if __package__ is None:
     DIR = Path(__file__).resolve().parent
     sys.path.insert(0, str(DIR.parent))
     __package__ = DIR.name
-    
+
 import os
 import re
 from typing import Set
@@ -15,9 +15,9 @@ import fire  # type: ignore
 from tqdm import tqdm  # type: ignore
 import rpyc  # type: ignore
 from pywebcopy import WebPage, config  # type: ignore
-from persistor import Persistor
-from graph import graph
-from utils import remove_dir, remove_symlink, download_url
+from .persistor import Persistor
+from .graph import graph
+from .utils import remove_dir, remove_symlink, download_url
 from __init__ import __version__
 
 
